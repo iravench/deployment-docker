28d7e690d4a86ebb68ce4346f7175dbddecc39ec
iravench <iravench@users.noreply.github.com>
2016-03-24 19:16:08 +1100

    describe policy

--- examples/msg-prototype/msg-session-broker/src/fm_policy.js
+++ examples/msg-prototype/msg-session-broker/src/fm_policy.js
@@ -1,5 +1,7 @@
 'use strict';
-
+// single user policy: maximum active session...
+// single server policy: maximum ws connection...
 export default {
-
+// inputs: known servers' current load
+// outputs: available front machine info
 }
